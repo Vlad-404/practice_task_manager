f36d951b75f7d385b4ea9d661980e198249b002c
Vlad-404 <vmijat21@gmail.com>
2020-08-05 12:30:08 +0000

    Added Procfile

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 if os.path.exists("env.py"):
     import env
 
+
 app = Flask(__name__)
 
 
@@ -13,5 +14,5 @@ def hello():
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-         port = int(os.environ.get("PORT")),
-         debug = True)
+         port=int(os.environ.get("PORT")),
+         debug=True)
